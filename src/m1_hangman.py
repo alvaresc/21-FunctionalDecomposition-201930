de4c6556ffe1bb956ab613d601e96c0e2e016f7f
alvaresc <alvaresc@rose-hulman.edu>
2019-04-26 10:21:36 -0400

    Done

--- src/m1_hangman.py
+++ src/m1_hangman.py
@@ -6,19 +6,11 @@ Authors: Jake Powell, Sam Alvares, Chloe Rife.
 Authors: PUT_YOUR_NAME_HERE and YOUR_PARTNERS_NAME_HERE.
 """  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
-# TODO: 2. Implement Hangman using your Iterative Enhancement Plan.
+# done: 2. Implement Hangman using your Iterative Enhancement Plan.
 
 import random
 ####### Do NOT attempt this assignment before class! #######
 
-#I will choose a random secret word from a dictionary.
-#You set the MINIMUM length of that word.
-#What MINIMUM length do you want for the secret word.
-#you set the DIFFICULTY of the gam by setting the number of UNSUCCESSFUL choices you can make before you LOSE the game.  The traditional form of Hangman sets this number to 5.
-#How many unsuccessful choices do you want to allow yourself?
-#Here is what you know about the secret word:
-#What letter do you want to try?
-#Sorr!
 
 
 def main():
@@ -27,7 +19,7 @@ def main():
     print()
     word_length = int(input('What MINIMUM length do you want for the secret word (less than 22 letters)?'))
     selected_word = random_word_gen(word_length)
-    print(selected_word)
+    #print(selected_word)
     tries_left=int(input('How many unsuccessful choices do you want to allow yourself?'))
     on_going_string=print_initial(len(selected_word))
     while True:
@@ -35,17 +27,21 @@ def main():
         in_or_not=check(guess_letter,selected_word)
         if in_or_not==False:
             tries_left = tries_left - 1
-            print('you have (below) tries left')
-            print(tries_left)
+            on_going_string = print_so_far(on_going_string,selected_word,guess_letter)
+            print()
+            print('You have {} unsuccessful guesses left'.format(tries_left))
+
         elif tries_left>0:
             on_going_string = print_so_far(on_going_string,selected_word,guess_letter)
-            print(tries_left)
+            print()
+            print('You have {} unsuccessful guesses left'.format(tries_left))
             if check_won(on_going_string,selected_word):
                 break
         if tries_left==0:
             print()
             print('You lost, sucks to suck')
             break
+        print('*****************************************************')
 
 
 
@@ -53,14 +49,17 @@ def main():
 
 def guess():
     guess_letter = str(input('What letter do you want to try?'))
+    print()
     return guess_letter
 
 def check(guess_letter,selected_word):
     for k in range(len(selected_word)):
         if selected_word[k] == guess_letter:
-            print('Yes, shes in there')
+            print('Yup, shes in there')
+            print()
             return True
-    print('nope')
+    print('Nope, try again!')
+    print()
     return False
 
 def random_word_gen(word_length):
@@ -90,7 +89,7 @@ def print_so_far(on_going_string,selected_word,guess_letter):
             new=new+on_going_string[k]
         else:
             new=new+'-'
-            print
+    print('Heres what ya got so far')
     print(new)
     return new
 
@@ -101,5 +100,8 @@ def check_won(on_going_string,selected_word):
         return True
 
 
-
-main()
+while True:
+    main()
+    play_again=str(input('Do you want to play again? Yes or No'))
+    if play_again == 'No':
+        break
